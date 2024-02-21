3dc9d5deb303568fefb5018eb978049ca561f554
Sam Jones <sam@refractar.com>
2024-02-21 14:27:15 -0700

    yep

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,21 @@ import "./App.css";
 
 const LENS_GROUP_ID = import.meta.env.VITE_LENS_GROUP_ID;
 const AUTH_TOKEN = import.meta.env.VITE_CAMERA_KIT_AUTH_ID;
-
+//be39b419-c314-4879-906a-7b4b8284f8c0
 function App() {
+  console.log("Auth Token", AUTH_TOKEN);
   const cameraKitRef = useRef(null);
   const canvasRef = useRef(null);
   const sessionRef = useRef(null);
 
   useEffect(() => {
     async function initCameraKit() {
-      const push2web = new Push2Web();
+      const push2Web = new Push2Web();
       const cameraKit = await bootstrapCameraKit(
         {
           apiToken: import.meta.env.VITE_CAMERA_KIT_API_KEY,
         },
-        (container) => container.provides(push2web.extension)
+        (container) => container.provides(push2Web.extension)
       );
       cameraKitRef.current = cameraKit;
 
@@ -30,25 +31,28 @@ function App() {
         LENS_GROUP_ID,
       ]);
 
-      console.log(lenses);
+      //console.log(lenses);
 
       const session = await cameraKit.createSession({
         liveRenderTarget: canvasRef.current,
       });
 
+      push2Web.subscribe(AUTH_TOKEN, session, cameraKit.lensRepository);
+
       // PUSH2WEB
-      // const push2web = new Push2Web();
-      // push2WebRef.current = push2web;
+      push2Web.events.addEventListener("error", (event) => {
+        console.error(event.detail);
+      });
 
-      // push2web.events.addEventListener("error", async (event) => {
-      //   const { id } = event.detail;
+      push2Web.events.addEventListener("lensReceived", async (event) => {
+        const { id } = event.detail;
 
-      //   const newLens = await cameraKitRef.current?.lensRepository.loadLens(
-      //     id,
-      //     LENS_GROUP_ID
-      //   );
-      //   await session.applyLens(newLens);
-      // });
+        const newLens = await cameraKitRef.current?.lensRepository.loadLens(
+          id,
+          LENS_GROUP_ID
+        );
+        await session.applyLens(newLens);
+      });
 
       sessionRef.current = session;
       session.events.addEventListener("error", (event) =>
@@ -65,6 +69,7 @@ function App() {
       await session.applyLens(lenses[8]);
       session.play();
     }
+
     if (!cameraKitRef.current) {
       initCameraKit();
     }
