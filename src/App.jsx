f72eaa9c7b6462bd397b8334d5c2dace7a47c8fc
Sam Jones <sam@refractar.com>
2024-02-21 12:14:58 -0700

    p2web setting up

--- src/App.jsx
+++ src/App.jsx
@@ -7,36 +7,22 @@ import {
 import { Push2Web } from "@snap/push2web";
 import "./App.css";
 
-const LENS_GROUP_ID = "542c15e5-1f57-450b-b0c6-f3f29df229aa";
+const LENS_GROUP_ID = import.meta.env.VITE_LENS_GROUP_ID;
+const AUTH_TOKEN = import.meta.env.VITE_CAMERA_KIT_AUTH_ID;
 
 function App() {
   const cameraKitRef = useRef(null);
   const canvasRef = useRef(null);
   const sessionRef = useRef(null);
-  const push2WebRef = useRef(null);
 
   useEffect(() => {
     async function initCameraKit() {
-      if (!push2WebRef.current) {
-        const push2web = new Push2Web();
-        push2WebRef.current = push2web;
-
-        push2web.events.addEventListener("error", async (event) => {
-          const { id } = event.detail;
-
-          const newLens = await cameraKitRef.current?.lensRepository.loadLens(
-            id,
-            LENS_GROUP_ID
-          );
-          await session.applyLens(newLens);
-        });
-      }
-
+      const push2web = new Push2Web();
       const cameraKit = await bootstrapCameraKit(
         {
           apiToken: import.meta.env.VITE_CAMERA_KIT_API_KEY,
         },
-        (container) => container.provides(push2WebRef.current)
+        (container) => container.provides(push2web.extension)
       );
       cameraKitRef.current = cameraKit;
 
@@ -49,6 +35,20 @@ function App() {
       const session = await cameraKit.createSession({
         liveRenderTarget: canvasRef.current,
       });
+
+      // PUSH2WEB
+      // const push2web = new Push2Web();
+      // push2WebRef.current = push2web;
+
+      // push2web.events.addEventListener("error", async (event) => {
+      //   const { id } = event.detail;
+
+      //   const newLens = await cameraKitRef.current?.lensRepository.loadLens(
+      //     id,
+      //     LENS_GROUP_ID
+      //   );
+      //   await session.applyLens(newLens);
+      // });
 
       sessionRef.current = session;
       session.events.addEventListener("error", (event) =>
